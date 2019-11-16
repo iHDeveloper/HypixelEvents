1e4b6b299d99a8fd3fecff2b03c14039921527a0
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2019-11-16 19:03:00 +0300

    Update the frame every 100 ms

--- src/app/timeline.ts
+++ src/app/timeline.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject, interval } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TimelineManager {
     events: Event[];
+    updater: Subject<void>;
 
     private renderFrame: () => void;
 
     public init(render: () => void): void {
         this.renderFrame = render;
+        if (this.updater !== undefined) {
+            this.updater.next();
+            this.updater.complete();
+        }
+        this.updater = new Subject<void>();
+        interval(100).pipe(takeUntil(this.updater)).subscribe(() => this.render());
         this.render();
     }
 
